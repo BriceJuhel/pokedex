5a97d6322bf3a586cc3d2c5064969fbb8f3c6d3b
Brice JUHEL <juhel.brice@gmail.com>
2023-08-11 15:34:08 +0200

    mise en place évolutions

--- pages/pokemon/[pokedexId].js
+++ pages/pokemon/[pokedexId].js
@@ -2,95 +2,114 @@ import Layout from '../../components/Layout';
 import Link from 'next/link';
 
 export default function PokemonPage({ pokemon }) {
-    return (
-        <Layout title={`Détails de ${pokemon.name}`}>
+  return (
+    <Layout title={`Détails de ${pokemon.name}`}>
       <div className="bg-white mx-auto max-w-7xl px-4 py-2 sm:px-6 sm:py-2 lg:px-8">
-        <div className="flex justify-end items-center mb-4">
-          </div>
-          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
-            <span className="font-semibold text-teal-600">#{pokemon.pokedexId}</span> {pokemon.name}
-          </h2>
-  
-          <div className="flex mt-4 space-x-4">
-            {pokemon.apiTypes.map((type) => (
-              <div key={type.name} className="flex items-center">
-                <img src={type.image} alt={type.name} className="w-5 h-5 mr-2 inline-block" />
-                <span>{type.name}</span>
-              </div>
-            ))}
-          </div>
-  
-          <hr className="my-6" />
-  
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
-            <div className="flex justify-center">
-              <img src={pokemon.image} alt={pokemon.name} className="w-full max-w-md object-contain" />
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+          <span className="font-semibold text-teal-600">#{pokemon.pokedexId}</span> {pokemon.name}
+        </h2>
+
+        <div className="flex mt-4 space-x-4">
+          {pokemon.apiTypes.map((type) => (
+            <div key={type.name} className="flex items-center">
+              <img src={type.image} alt={type.name} className="w-5 h-5 mr-2 inline-block" />
+              <span>{type.name}</span>
             </div>
-            <div className="space-y-6">
-              <h3 className="text-xl font-bold text-gray-900">Statistiques de base :</h3>
-              <ul className="list-disc list-inside list-none">
-                <li>
-                  <span className="font-bold">PV : {pokemon.stats.HP}</span>
-                  <div className="w-full h-4 rounded-md mt-1 bg-gray-200">
-                    <div
-                      className="h-full bg-teal-400 rounded-md"
-                      style={{ width: `${(pokemon.stats.HP / 255) * 100}%` }}
-                    ></div>
-                  </div>
-                </li>
-                <li>
-                  <span className="font-bold">Attaque : {pokemon.stats.attack}</span>
-                  <div className="w-full h-4 rounded-md mt-1 bg-gray-200">
-                    <div
-                      className="h-full bg-teal-400 rounded-md"
-                      style={{ width: `${(pokemon.stats.attack / 255) * 100}%` }}
-                    ></div>
-                  </div>
-                </li>
-                <li>
-                  <span className="font-bold">Défense : {pokemon.stats.defense}</span>
-                  <div className="w-full h-4 rounded-md mt-1 bg-gray-200">
-                    <div
-                      className="h-full bg-teal-400 rounded-md"
-                      style={{ width: `${(pokemon.stats.defense / 255) * 100}%` }}
-                    ></div>
-                  </div>
-                </li>
-                <li>
-                  <span className="font-bold">Attaque Spéciale : {pokemon.stats.special_attack}</span>
-                  <div className="w-full h-4 rounded-md mt-1 bg-gray-200">
-                    <div
-                      className="h-full bg-teal-400 rounded-md"
-                      style={{ width: `${(pokemon.stats.special_attack / 255) * 100}%` }}
-                    ></div>
-                  </div>
-                </li>
-                <li>
-                  <span className="font-bold">Défense Spéciale : {pokemon.stats.special_defense}</span>
-                  <div className="w-full h-4 rounded-md mt-1 bg-gray-200">
-                    <div
-                      className="h-full bg-teal-400 rounded-md"
-                      style={{ width: `${(pokemon.stats.special_defense / 255) * 100}%` }}
-                    ></div>
-                  </div>
-                </li>
-                <li>
-                  <span className="font-bold">Vitesse : {pokemon.stats.speed}</span>
-                  <div className="w-full h-4 rounded-md mt-1 bg-gray-200">
-                    <div
-                      className="h-full bg-teal-400 rounded-md"
-                      style={{ width: `${(pokemon.stats.speed / 255) * 100}%` }}
-                    ></div>
-                  </div>
-                </li>
-                {/* Ajoutez les autres statistiques ici */}
-              </ul>
-            </div>
+          ))}
+        </div>
+
+        <hr className="my-6" />
+
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+          <div className="flex justify-center">
+            <img src={pokemon.image} alt={pokemon.name} className="w-full max-w-md object-contain" />
+          </div>
+          <div className="space-y-6">
+            <h3 className="text-xl font-bold text-gray-900">Statistiques de base :</h3>
+            <ul className="list-disc list-inside list-none">
+              <li>
+                <span className="font-bold">PV : {pokemon.stats.HP}</span>
+                <div className="w-full h-4 rounded-md mt-1 bg-gray-200">
+                  <div
+                    className="h-full bg-teal-400 rounded-md"
+                    style={{ width: `${(pokemon.stats.HP / 255) * 100}%` }}
+                  ></div>
+                </div>
+              </li>
+              <li>
+                <span className="font-bold">Attaque : {pokemon.stats.attack}</span>
+                <div className="w-full h-4 rounded-md mt-1 bg-gray-200">
+                  <div
+                    className="h-full bg-teal-400 rounded-md"
+                    style={{ width: `${(pokemon.stats.attack / 255) * 100}%` }}
+                  ></div>
+                </div>
+              </li>
+              <li>
+                <span className="font-bold">Défense : {pokemon.stats.defense}</span>
+                <div className="w-full h-4 rounded-md mt-1 bg-gray-200">
+                  <div
+                    className="h-full bg-teal-400 rounded-md"
+                    style={{ width: `${(pokemon.stats.defense / 255) * 100}%` }}
+                  ></div>
+                </div>
+              </li>
+              <li>
+                <span className="font-bold">Attaque Spéciale : {pokemon.stats.special_attack}</span>
+                <div className="w-full h-4 rounded-md mt-1 bg-gray-200">
+                  <div
+                    className="h-full bg-teal-400 rounded-md"
+                    style={{ width: `${(pokemon.stats.special_attack / 255) * 100}%` }}
+                  ></div>
+                </div>
+              </li>
+              <li>
+                <span className="font-bold">Défense Spéciale : {pokemon.stats.special_defense}</span>
+                <div className="w-full h-4 rounded-md mt-1 bg-gray-200">
+                  <div
+                    className="h-full bg-teal-400 rounded-md"
+                    style={{ width: `${(pokemon.stats.special_defense / 255) * 100}%` }}
+                  ></div>
+                </div>
+              </li>
+              <li>
+                <span className="font-bold">Vitesse : {pokemon.stats.speed}</span>
+                <div className="w-full h-4 rounded-md mt-1 bg-gray-200">
+                  <div
+                    className="h-full bg-teal-400 rounded-md"
+                    style={{ width: `${(pokemon.stats.speed / 255) * 100}%` }}
+                  ></div>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
-      </Layout>
-    );
-  }
+
+              {pokemon.apiEvolutions && pokemon.apiEvolutions.length > 0 && (
+                <div className="mt-10">
+                  <h3 className="text-xl font-bold text-gray-900">Évolution(s) :</h3>
+                  <div className="flex items-center space-x-4">
+                    {pokemon.apiEvolutions.map((evolution) => (
+                      <div key={evolution.name} className="flex items-center">
+                        <Link legacyBehavior href={`/pokemon/${evolution.pokedexId}`}>
+                          <a>
+                            <img
+                              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${evolution.pokedexId}.png`}
+                              alt={evolution.name}
+                              className="w-30 h-30 mr-2"
+                            />
+                          </a>
+                        </Link>
+                        <span>{evolution.name}</span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+      </div>
+    </Layout>
+  );
+}
 
   export async function getStaticPaths() {
     const res = await fetch('https://pokebuildapi.fr/api/v1/pokemon/');
@@ -131,6 +150,7 @@ async function fetchPokemonData(pokedexId) {
       sprite: data.sprite,
       apiTypes: data.apiTypes || [], // Assurez-vous que les données des types sont correctement récupérées depuis l'API
       stats: data.stats,
+      apiEvolutions: data.apiEvolutions || [],
       apiResistances: data.apiResistances || [], // Assurez-vous que les données de résistance sont correctement récupérées depuis l'API
       // Ajoutez d'autres propriétés ici si nécessaire
     };
